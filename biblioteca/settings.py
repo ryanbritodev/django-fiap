38e88fd4e3832bb26e1bd0f187e5813496751980
Ryan Brito <rm554497@fiap.com.br>
2025-03-04 10:53:54 -0300

    🔨 fix: SQLite db error fixed

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -84,10 +84,9 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
