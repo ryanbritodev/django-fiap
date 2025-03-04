46501d6972a27a2e3c8ee145f9fb968b7106c3c9
Ryan Brito <rm554497@fiap.com.br>
2025-03-04 11:08:35 -0300

    🐘 update: PostgreSQL migration

--- biblioteca/wsgi.py
+++ biblioteca/wsgi.py
@@ -8,18 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biblioteca.settings')
-django.setup()
-
-from django.core.management import call_command
-
-# Criar/migrar banco de dados ao iniciar a aplicação
-if not os.path.exists('/tmp/db.sqlite3'):
-    call_command('migrate')
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-app = application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biblioteca.settings')
+
+application = get_wsgi_application()
+
+app = application
