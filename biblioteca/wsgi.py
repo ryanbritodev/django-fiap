842b29324b86711dd07f8f0c9db61cc8601fe3c6
Ryan Brito <rm554497@fiap.com.br>
2025-03-04 11:20:50 -0300

    🔨 fix: Superuser creation

--- biblioteca/wsgi.py
+++ biblioteca/wsgi.py
@@ -14,12 +14,26 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biblioteca.settings')
 django.setup()
 
 from django.core.management import call_command
+from django.contrib.auth import get_user_model
 
-# Criar/migrar banco de dados ao iniciar a aplicação
+# Criar/migrar banco de dados automaticamente
 if not os.path.exists('/tmp/db.sqlite3'):
-    call_command('migrate')
+    call_command('migrate')  # Aplica as migrações
+
+# Criar superusuário automaticamente se ele não existir
+User = get_user_model()
+
+try:
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@admin.com',
+            password='admin'
+        )
+        print("Superusuário criado com sucesso!")
+except Exception as e:
+    print(f"Erro ao criar superusuário: {e}")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 app = application
-
