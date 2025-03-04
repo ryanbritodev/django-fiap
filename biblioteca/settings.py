b75281a8f53857973944ba4241769b123a51eb23
Ryan Brito <rm554497@fiap.com.br>
2025-03-04 11:29:18 -0300

    🔨 fix: Old DB functionalities

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('/tmp', 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- biblioteca/wsgi.py
+++ biblioteca/wsgi.py
@@ -8,32 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biblioteca.settings')
-django.setup()
-
-from django.core.management import call_command
-from django.contrib.auth import get_user_model
-
-# Criar/migrar banco de dados automaticamente
-if not os.path.exists('/tmp/db.sqlite3'):
-    call_command('migrate')  # Aplica as migrações
-
-# Criar superusuário automaticamente se ele não existir
-User = get_user_model()
-
-try:
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser(
-            username='admin',
-            email='admin@admin.com',
-            password='admin'
-        )
-        print("Superusuário criado com sucesso!")
-except Exception as e:
-    print(f"Erro ao criar superusuário: {e}")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biblioteca.settings')
+
 application = get_wsgi_application()
+
 app = application
